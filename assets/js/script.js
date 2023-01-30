420dec370e0906a0029997e4d8a37d0b167aff7d
yeasin13 <yeasinarafat8001@gmail.com>
2023-01-30 15:27:42 +0600

    parallax-elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,3 +20,18 @@ const addEventOnElement = function (elements, eventType, callback) {
  }
 
  addEventOnElement([navToggleBtn, overlay], "click", toggleNavbar);
+
+//  Parallax effect 
+
+const parallaxElements = document.querySelectorAll("[data-parallax]");
+
+window.addEventListener("mousemove", event => {
+    for (let i = 0, len = parallaxElements.length; i < len; i++) {
+        const movementX = (event.clientX / window.innerWidth) * Number (parallaxElements[i].dataset.parallaxSpeed);
+        const movementY = (event.clientY / window.innerHeight) * Number (parallaxElements[i].dataset.parallaxSpeed);
+
+        parallaxElements[i].animate({
+            transform: `translate(${movementX}px, ${movementY}px)`
+        }, {duration:500, fill: "forwards"})
+    }
+})
